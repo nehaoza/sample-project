be907ad11fb8d33950910204d66f062fe6b61305
Neha <nehaoza1307@gmail.com>
2023-04-03 01:39:09 +0100

    initial commit

--- src/main/java/com/sample/project/PersonService.java
+++ src/main/java/com/sample/project/PersonService.java
@@ -1,11 +1,12 @@
 package com.sample.project;
 
+import com.sample.project.domain.Person;
 import com.sample.project.exception.InvalidAgeException;
 
 public class PersonService {
 
-    public void verifyAge(int age) throws InvalidAgeException {
-        if(age < 18) {
+    public void verify(Person person) throws InvalidAgeException {
+        if (person.age() < 18) {
             throw new InvalidAgeException("Person should be at least 18 years old");
         }
     }
--- src/test/java/com/sample/project/PersonServiceTest.java
+++ src/test/java/com/sample/project/PersonServiceTest.java
@@ -1,17 +1,20 @@
 package com.sample.project;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.sample.project.domain.Person;
+import com.sample.project.domain.PersonBuilder;
 import com.sample.project.exception.InvalidAgeException;
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.*;
 
 public class PersonServiceTest {
 
     @Test
     public void shouldThrowWhenInvalidAgeIsPassed() {
         PersonService service = new PersonService();
+        Person below18Years = PersonBuilder.builder().age(17).name("xyz").build();
         assertThatExceptionOfType(InvalidAgeException.class)
-            .isThrownBy(() -> service.verifyAge(2))
-            .withMessage("Person should be at least 18 years old");
+                .isThrownBy(() -> service.verify(below18Years))
+                .withMessage("Person should be at least 18 years old");
     }
 }
