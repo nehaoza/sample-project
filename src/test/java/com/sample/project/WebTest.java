3fa4212b9a2870e9d13122ff38f90bd1f54644dc
Neha <nehaoza1307@gmail.com>
2025-03-24 21:54:41 +0000

    testing setup

--- src/test/java/com/sample/project/WebTest.java
+++ src/test/java/com/sample/project/WebTest.java
@@ -1,4 +1,33 @@
 package com.sample.project;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebTest {
+
+
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+    @Test
+    public void test() {
+        driver = new ChromeDriver();
+
+        driver.get("https://www.flipkart.com/");
+
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void teardown() {
+        driver.close();
+    }
 }
